61ac1c109400782d9397d96b5f98b99f5742ca11
jeroen-huizer-conclusion <jhuizer@conclusion.nl>
2017-09-22 11:50:33 +0200

    Atom ESLinter screwed formatting up. Also fixed a bug where the tooltip would not hide in IE when the attached node was destroyed.

--- src/HelpTextTooltip/widget/HelpTextTooltip.js
+++ src/HelpTextTooltip/widget/HelpTextTooltip.js
@@ -1,97 +1,125 @@
-/* global define, require, mx*/ 
+/* global define, require, mx*/
 "use strict";
 
 define([
-    "dojo/_base/declare",
-    "mxui/widget/_WidgetBase",
+        "dojo/_base/declare",
+        "mxui/widget/_WidgetBase",
 
-    "dojo/_base/lang",
-    "dojo/dom-construct",
+        "dojo/_base/lang",
+        "dojo/dom-construct",
 
-    "dijit/Tooltip"
-    ], 
-    function (declare, _WidgetBase, lang, domConstruct, Tooltip) {
+        "dijit/Tooltip"
+    ],
+    function(declare, _WidgetBase, lang, domConstruct, Tooltip) {
 
-    return declare("HelpTextTooltip.widget.HelpTextTooltip", [_WidgetBase], {
+        return declare("HelpTextTooltip.widget.HelpTextTooltip", [
+            _WidgetBase
+        ], {
 
-        // Widget
-        contexAttribute: null,
-        helpEntity: null,
-        keyAttribute: null,
-        helpAttribute: null,
-        displayIcon: true,
+            // Widget
+            contexAttribute: null,
+            helpEntity: null,
+            keyAttribute: null,
+            helpAttribute: null,
+            displayIcon: true,
 
-        // Local
-        _contextObj: {},
-        _helpText : '',
+            // Local
+            _contextObj: {},
+            _helpText: '',
+            _tooltip: {},
 
-        postCreate: function(){
-            if(this.domNode)
-                this.domNode.style.display = "none"; // Don't want to use space
-        },
+            postCreate: function() {
+                if (this.domNode)
+                    this.domNode.style.display = "none"; // Don't want to use space
+            },
 
-        update: function (obj, callback) {
-            if(obj){
-                this._contextObj = obj;
-            }
-            if(callback){ callback(); }
-        },
-
-        _getHelpTextInBackground: function(node){
-            // There might be a better way to 'act rendered', but I don't know it
-            var that = this; 
-            setTimeout(function(){that._getHelpText(node);}, 1);
-        },
-
-        _getHelpText: function(node){
-
-            if(!this._helpText.length){
-                var xpath = "//"+this.helpEntity+this.keyConstraint;
-                if(this.keyConstraint.indexOf('CurrentObject') > 0 && this._contextObj){
-                    xpath.replace(/\[\%CurrentObject\%\]/gi, this._contextObj.getGuid());
+            update: function(obj, callback) {
+                if (obj) {
+                    this._contextObj = obj;
                 }
-                var filter = {attributes: [this.helpAttribute], amount: 1};
-                mx.data.get({xpath: xpath, callback: lang.hitch(this, this._setHelpText, node), filter: filter});
-            }
-            else{
-                this._setHelpText(node);
-            }
-        },
+                if (callback) {
+                    callback();
+                }
+            },
 
-        _setHelpText: function(node, objs){
-            if(objs && objs.length){
-                var text = objs[0].get(this.helpAttribute);
-                this._helpText = text.replace(/(?:\r\n|\r|\n)/g, '<br/>');
-            } 
+            uninitialize: function() {
+                this._tooltip.close(); // In IE: Tooltip does *not always* close when connected node is destroyed.
+                this._tooltip.destroy();
+            },
 
-            this._updateRendering(node);
-        },
+            _getHelpTextInBackground: function(node) {
+                // There might be a better way to 'act rendered', but I don't know it
+                var that = this;
+                setTimeout(function() {
+                    that._getHelpText(node);
+                }, 1);
+            },
 
-        _updateRendering: function(node){
-            if(!this._helpText || !this._helpText.length)
+            _getHelpText: function(node) {
+
+                if (!this._helpText.length) {
+                    var xpath = "//" + this.helpEntity + this.keyConstraint;
+                    if (this.keyConstraint.indexOf(
+                            'CurrentObject') > 0 && this._contextObj) {
+                        xpath.replace(/\[\%CurrentObject\%\]/gi,
+                            this._contextObj.getGuid());
+                    }
+                    var filter = {
+                        attributes: [this.helpAttribute],
+                        amount: 1
+                    };
+                    mx.data.get({
+                        xpath: xpath,
+                        callback: lang.hitch(this, this
+                            ._setHelpText, node),
+                        filter: filter
+                    });
+                } else {
+                    this._setHelpText(node);
+                }
+            },
+
+            _setHelpText: function(node, objs) {
+                if (objs && objs.length) {
+                    var text = objs[0].get(this.helpAttribute);
+                    this._helpText = text.replace(
+                        /(?:\r\n|\r|\n)/g, '<br/>');
+                }
+
+                this._updateRendering(node);
+            },
+
+            _updateRendering: function(node) {
+                if (!this._helpText || !this._helpText.length)
                     return;
 
-            this._addTooltipToNode(this._helpText, node);
+                this._addTooltipToNode(this._helpText, node);
 
-            // node.setAttribute("title", this._helpText);
-            if(this.displayIcon){
-                var supNode = domConstruct.create("sup",{style:"padding-left:3px;"}, node);
-                domConstruct.create("div", {class: "glyphicon glyphicon-question-sign"}, supNode);
+                // node.setAttribute("title", this._helpText);
+                if (this.displayIcon) {
+                    var supNode = domConstruct.create("sup", {
+                        style: "padding-left:3px;"
+                    }, node);
+                    domConstruct.create("div", {
+                        class: "glyphicon glyphicon-question-sign"
+                    }, supNode);
+                }
+            },
+
+            _addTooltipToNode: function(tooltipText, node) {
+                // Overwrites all tooltips..
+                Tooltip.defaultPosition = ['below-alt', 'below',
+                    'after', 'before', 'above-alt', 'above'
+                ];
+
+                this._tooltip = new Tooltip({
+                    label: this._helpText,
+                    showDelay: 250,
+                    connectId: [node]
+                });
+
             }
-        },
-
-        _addTooltipToNode: function(tooltipText, node){
-            // Overwrites all tooltips..
-            Tooltip.defaultPosition = ['below-alt','below', 'after', 'before', 'above-alt', 'above'];
-
-            new Tooltip({
-                label: this._helpText,
-                showDelay: 250,
-                connectId: [node]
-            });
-
-        }
+        });
     });
-});
 
 require(["HelpTextTooltip/widget/HelpTextTooltip"]);
